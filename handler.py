2b916045317af80e18d9307a511123556b757659
j.du <julien.dubiel@aphp.fr>
2018-05-03 17:26:52 +0200

    Remove useless files

--- handler.py
+++
@@ -1,6 +0,0 @@
-import re
-
-
-def handle_query(query):
-    if re.match(pattern=' *SELECT +( *[\w|-]+ *, *)* *[\w|-]+ * +WHERE +\(( *( *[\w|-]+(?:==|>=|<=)[\w|-]+ *,)* *[\w|-]+(?:==|>=|<=)[\w|-]+)*\) *', string=query, flags=re.IGNORECASE):
-        pass
